a7f23e628cf76e2456adc0a634f2ae9104a748c5
Javialex95 <alexjls95@gmail.com>
2022-11-28 16:32:05 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,16 +17,16 @@ const Footer = ({ color }) => {
     <div className="footer">
       <div className="footer_container">
         <div className="footer_container_items">
-          <a target="_blank" href="" className="ga4-call_to_action-redes_footer">
+          <a target="_blank" href="https://www.youtube.com/@CantiRondas" className="ga4-call_to_action-redes_footer">
             <div className="footer_container_items_youtube"></div>
           </a>
-          <a target="_blank" href="" className="ga4-call_to_action-redes_footer">
+          <a target="_blank" href="https://open.spotify.com/user/0nmf4hucm0cto99is31curkmr?si=19998a14fcc24673" className="ga4-call_to_action-redes_footer">
             <div className="footer_container_items_spotify"></div>
           </a>
-          <a target="_blank" href="" className="ga4-call_to_action-redes_footer">
+          <a target="_blank" href="https://www.instagram.com/canti.rondas/" className="ga4-call_to_action-redes_footer">
             <div className="footer_container_items_instagram"></div>
           </a>
-          <a target="_blank" href="" className="ga4-call_to_action-redes_footer">
+          <a target="_blank" href="https://wgitww.facebook.com/cantirondas" className="ga4-call_to_action-redes_footer">
             <div className="footer_container_items_facebook"></div>
           </a>
         </div>
